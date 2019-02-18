398fa694d2dd66ce149da2fa139fa2bd60658db6
pbsarathy21 <pbsarathy21@gmail.com>
2019-02-18 15:09:33 +0530

    FEB 18, 2019 03:05 PM
    maps added

--- app/src/main/java/com/ninositsolution/inveleapp/my_order/MyOrderActivity.java
+++ app/src/main/java/com/ninositsolution/inveleapp/my_order/MyOrderActivity.java
@@ -7,14 +7,21 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 import com.ninositsolution.inveleapp.R;
 import com.ninositsolution.inveleapp.databinding.ActivityMyOrderBinding;
 import com.ninositsolution.inveleapp.product_review.ProductReviewActivity;
 
-public class MyOrderActivity extends AppCompatActivity implements IMyOrder{
+public class MyOrderActivity extends AppCompatActivity implements IMyOrder, OnMapReadyCallback {
 
     ActivityMyOrderBinding binding;
     BottomSheetBehavior bottomSheetBehavior_delivered, bottomSheetBehavior_storepick;
+    private GoogleMap mMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +31,10 @@ public class MyOrderActivity extends AppCompatActivity implements IMyOrder{
         binding.setMyOrder(new MyOrderVM(getApplicationContext(), this));
         bottomSheetBehavior_delivered = BottomSheetBehavior.from(binding.bottomSheetTrackorder);
         bottomSheetBehavior_storepick = BottomSheetBehavior.from(binding.bottomSheetStore);
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
     }
 
     @Override
@@ -50,11 +61,13 @@ public class MyOrderActivity extends AppCompatActivity implements IMyOrder{
 
             if (position%2 == 0)
             {
-                if (bottomSheetBehavior_storepick.getState() == BottomSheetBehavior.STATE_EXPANDED)
+                if (bottomSheetBehavior_storepick.getState() == BottomSheetBehavior.STATE_HALF_EXPANDED)
                     bottomSheetBehavior_storepick.setState(BottomSheetBehavior.STATE_COLLAPSED);
 
                 else
                     bottomSheetBehavior_storepick.setState(BottomSheetBehavior.STATE_EXPANDED);
+
+               /* startActivity(new Intent(this, MapsActivity.class));*/
             }
                else
             {
@@ -65,10 +78,15 @@ public class MyOrderActivity extends AppCompatActivity implements IMyOrder{
                     bottomSheetBehavior_delivered.setState(BottomSheetBehavior.STATE_EXPANDED);
             }
 
+    }
 
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
 
-      /*  startActivity(new Intent(this, MapsActivity.class));*/
-
-
+        // Add a marker in Sydney and move the camera
+        LatLng singapore = new LatLng(1.3521, 103.8198);
+        mMap.addMarker(new MarkerOptions().position(singapore).title("Singapore"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(singapore));
     }
 }
